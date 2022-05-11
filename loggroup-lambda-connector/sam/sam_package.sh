33a6866137a91f92cf34c4841a0cb66658ef4d96
Joel Walden <joel.a.walden@protonmail.com>
2022-05-10 18:33:46 -0600

    Lambda ARN updated

--- loggroup-lambda-connector/sam/sam_package.sh
+++ loggroup-lambda-connector/sam/sam_package.sh
@@ -2,7 +2,6 @@
 
 SAM_S3_BUCKET="kbs-sumologic-connector"
 AWS_REGION="us-west-2"
-LAMBDA_ARN="arn:aws:lambda:us-west-2:529513974030:function:SumoCWLogsLambda-33c639e0-1d57-11e9-ae4a-0ae846f1e916"
 LOG_GROUP_PATTERN="\/aws\/(lambda|fargate)\/(?!(kanaha|rdb|wasaga|.+?_producer_|workday\-producers)).+"
 
 version="1.0.4"
@@ -11,7 +10,7 @@ sam package --template-file template.yaml --s3-bucket $SAM_S3_BUCKET  --output-t
 
 # sam deploy --template-file packaged.yaml --stack-name testingloggrpconnector --capabilities CAPABILITY_IAM --region $AWS_REGION  --parameter-overrides LambdaARN="arn:aws:lambda:us-east-1:956882708938:function:SumoCWLogsLambda" LogGroupTags="env=prod,name=apiassembly" LogGroupPattern="test"
 
-sam deploy --no-execute-changeset --capabilities CAPABILITY_IAM --stack-name serverlessrepo-sumologic-loggroup-connector --template packaged.yaml --region $AWS_REGION --parameter-overrides LogGroupPattern=$LOG_GROUP_PATTERN LambdaARN=$LAMBDA_ARN DestinationArnValue="arn:aws:lambda:us-west-2:529513974030:function:serverlessrepo-sumologic-SumoLogGroupLambdaConnec-1MMCQVW6C6OEO"
+sam deploy --no-execute-changeset --capabilities CAPABILITY_IAM --stack-name serverlessrepo-sumologic-loggroup-connector --template packaged.yaml --region $AWS_REGION --parameter-overrides LogGroupPattern=$LOG_GROUP_PATTERN UseExistingLogs="true" DestinationArnValue="arn:aws:lambda:us-west-2:529513974030:function:SumoCWLogsLambda-33c639e0-1d57-11e9-ae4a-0ae846f1e916"
 # aws cloudformation describe-stack-events --stack-name testingloggrpconnector --region $AWS_REGION
 # aws cloudformation get-template --stack-name testingloggrpconnector  --region $AWS_REGION
 
